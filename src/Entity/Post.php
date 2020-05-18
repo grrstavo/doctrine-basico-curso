98a218cff85f1a1565dc47b23078cced1466e97b
Gustavo_Sales <gustavo.sales@liveecommerce.com.br>
2020-05-17 21:04:36 -0300

    Model de Post

---
+++ src/Entity/Post.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * @Entity
+ * @Table(name="posts")
+ */
+class Post
+{
+    /**
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @Column(type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+}
